bea51e314289086892030b6d05d8a745608b1611
Jay j <j.jayachandran@gmail.com>
2025-10-27 18:05:55 +0000

    web app option is not available in some of the ios iphones

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,9 @@ import { Toaster } from "@/components/ui/toaster";
 import { FirebaseClientProvider } from '@/firebase/client-provider';
 
 export const metadata: Metadata = {
-  title: 'news',
+  title: 'CipherChat',
   description: 'A personal chat application with message scrambling',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -19,6 +20,13 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+        <meta name="application-name" content="CipherChat" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="CipherChat" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#000000" />
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>
